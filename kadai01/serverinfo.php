55d9d3e062dbbe718726a87cb9775ef92dd8a101
nashi2603 <nashi.20306.y@gmail.com>
2019-10-19 04:54:21 +0900

    時刻をフォーマットして表示できるように

--- kadai01/serverinfo.php
+++ kadai01/serverinfo.php
@@ -2,6 +2,8 @@
     foreach(array("SCRIPT_FILENAME", "SERVER_NAME", "REQUEST_METHOD", "REQUEST_TIME", "REMOTE_ADDR", "REMOTE_HOST", "REMOTE_PORT") as $i) {
         if ($i == "REMOTE_HOST") {
             echo gethostbyaddr($_SERVER["REMOTE_ADDR"]) . "<br>";
+        } elseif ($i == "REQUEST_TIME") {
+            echo $_SERVER[$i] . " (". date("Y/m/d h/m/s", $_SERVER[$i]) .")<br>";
         } else {
             echo $_SERVER[$i] . "<br>";
         }
