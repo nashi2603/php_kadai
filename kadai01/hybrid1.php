77bef5f9d3bd7b86f7cda041d36117296bd73e4b
nashi2603 <nashi.20306.y@gmail.com>
2019-10-21 04:20:16 +0900

    hybrid1にAjaxで自動更新をかけるようにした

--- kadai01/hybrid1.php
+++ kadai01/hybrid1.php
@@ -2,8 +2,7 @@
 <html lang="ja">
     <head>
         <meta charset="UTF-8">
-        <meta http-equiv="refresh" content="1;URL='./hybrid1.php'">
-        <!-- ページ表示したときだけしか動かない(はず)ので多少は許してください！ -->
+        <!-- <meta http-equiv="refresh" content="1;URL='./hybrid1.php'"> -->
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     </head>
@@ -11,20 +10,30 @@
         <script src="https://code.jquery.com/jquery-3.3.1.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+        <script src="./.prototype.js"></script>
         <script>
-            $(function () {
-                $.ajax({
-                    url: "hybrid1.php",
-                    type: 'POST',
-                    data: {val:"hybrid1"}
-                    timeout: 10000,
-                    dataType: 'Text'
-                }).done(function()) {
-                    $("#ajax").html(data);
-                })
-            });
+            function reloadpage() {
+                var reloader_post = new Ajax.PeriodicalUpdater(
+                    'hybrid1',
+                    './hybrid1.php',
+                    {
+                        "method": "post",
+                        frequency: 0.5,
+                        onSuccess: function(request) {
+                            console.log('Reload');
+                        },
+                        onFailure: function(request) {
+                            console.log('Failed');
+                        },
+                        onException: function(request) {
+                            console.log('Exception');
+                        }
+                    }
+                );
+            }
+            reloadpage();
         </script>
-        <div class="text-center m-5">
+        <div class="text-center m-5" id="hybrid1">
         <?php
             if (isset($_COOKIE['count'])) {
                 setcookie("count", $_COOKIE['count']+1);
@@ -40,7 +49,8 @@
                 if ($_COOKIE['count'] == 8) {
                     setcookie("count", 1);
                 }
-                echo "<div class=\"progress mt-3\"><div class=\"progress-bar progress-bar-striped progress-bar-animated\" style=\"width:". (12.5 * $_COOKIE['count']) ."%\" role=\"progressbar\" aria-valuenow=\"". (125 * $_COOKIE['count']) ."\" aria-valuemin=\"0\" aria-valuemax=\"1000\"></div></div>";
+                // echo "<div class=\"progress mt-3\"><div class=\"progress-bar progress-bar-striped progress-bar-animated\" style=\"width:". (12.5 * $_COOKIE['count']) ."%\" role=\"progressbar\" aria-valuenow=\"". (125 * $_COOKIE['count']) ."\" aria-valuemin=\"0\" aria-valuemax=\"1000\"></div></div>";
+                // プログレスバー作ったけどなんかキモかったからやめた(コメント化解除でプログレスバー)
             } else {
                 setcookie("count", 0);
             }
