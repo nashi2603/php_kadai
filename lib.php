1b671acab3e82a5d0ebae30ce010ee183a244e5f
nashi2603 <nashi.20306.y@gmail.com>
2019-11-26 12:30:52 +0900

    11/26

--- lib.php
+++ lib.php
@@ -9,3 +9,24 @@ function dbcon() {
     }
     return $pdo;
 }
+
+// 指定されたテーブルのデータをすべて表示する
+function show($pdo, $t) {
+    $sql = "SELECT * FROM ${t}";
+    $res = $pdo->query($sql);
+    $data = $res->fetchAll();
+    echo "<table border=\"1\">";
+    echo "<tr>";
+    for ($i = 0; $col = $res->getColumnMeta($i); $i++) {
+        echo "<th>".$col["name"]."</th>";
+    }
+    echo "</tr>";
+    foreach ($data as $rows) {
+        echo "<tr>";
+        foreach ($rows as $v) {
+            echo "<td>".$v."</td>";
+        }
+        echo "</tr>";
+    }
+    echo "</table>";
+}
--- pdo_select.php
+++ pdo_select.php
@@ -5,12 +5,26 @@ require_once "lib.php";
 
 $pdo = dbcon();
 
-$sql = "SELECT * FROM pokomon";
+$sql = "SELECT * FROM pokemon";
 $res = $pdo->query($sql);
+// var_dump($res);
 
 // 見出しだけ表示
 for ($i = 0; $col = $res->getColumnMeta($i); $i++) {
     echo $col['name'] . ",";
 }
+
+$data = $res->fetchAll();
+foreach($data as $rows) {
+    // var_dump($rows);
+    echo $rows["id"];
+    echo $rows["name"];
+    echo $rows["type"];
+    echo $rows["update"];
+}
+
+$tbl = "pokemon";
+show($pdo, $tbl);
+
 $pdo = null;
 ?>
